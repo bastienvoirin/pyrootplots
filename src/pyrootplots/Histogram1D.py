16934f11fbf80eead48ebb06e196a85f88e61df7
Bastien Voirin <bastien.voirin@ens-lyon.fr>
2022-07-12 10:47:35 +0200

    Debug weights in Histogram1D

--- src/pyrootplots/Histogram1D.py
+++ src/pyrootplots/Histogram1D.py
@@ -122,12 +122,12 @@ class Histogram1D:
                                loc      = ylabelloc,
                                fontsize = xylabelfontsize)
 
-        print(*[d.shape for d in self.data], sep=",")
-        print(*[w.shape for w in self.weights], sep=",")
+        print(*[d.shape for d in np.array(self.data)], sep=",")
+        print(*[w.shape for w in np.array(self.weights)], sep=",")
         for i in range(len(self.weights)):
             if self.weights[i] is 1:
                 self.weights[i] = self.unitWeight(self.data[i].shape[0], i)
-        print(*[w.shape for w in self.weights], sep=",")
+        print(*[w.shape for w in np.array(self.weights)], sep=",")
 
         mergedData    = pd.concat(self.data[:],    axis=1)
         mergedWeights = pd.concat(self.weights[:], axis=1)

0d629e986d6c7add24f89d5edced8547d66223b1
Bastien Voirin <bastien.voirin@ens-lyon.fr>
2022-07-12 13:48:40 +0200

    Debug weights in Histogram1D and add Color.py

---
+++ src/pyrootplots/Color.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+from enum import Enum
+
+class BaseColor(Enum):
+    """BaseColor.
+
+    """
+    RED     = 0
+    ORANGE  = 1
+    YELLOW  = 2
+    SPRING  = 3
+    GREEN   = 4
+    TEAL    = 5
+    CYAN    = 6
+    AZURE   = 7
+    BLUE    = 8
+    VIOLET  = 9
+    MAGENTA = 10
+    PINK    = 11
+    GRAY    = 12
+    WHITE   = 13
+    BLACK   = 14
+
+class Color:
+    def __init__(self):
+        """Color.
+
+        """
+
+        self.
+
+    def __str__(self):
+        """Concise string representation of an instance."""
+        return "Color(...)" # TODO>
+
+    def __repr__(self):
+        """Complete string representation of an instance."""
+        return "Color(...)" # TODO
+
+    def __add__(self, shift):
+        return
+
+    def __sub__(self, shift):
+        return

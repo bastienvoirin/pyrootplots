376b95e0bdbdbceb8de237a9f4946726f440df1d
Bastien Voirin <bastien.voirin@ens-lyon.fr>
2022-07-13 13:00:10 +0200

    Fix typo

--- src/pyrootplots/Histogram1D.py
+++ src/pyrootplots/Histogram1D.py
@@ -183,7 +183,7 @@ class Histogram1D:
             mergedScaledData = pd.concat([data for (scale, data) in zip(self.scale, self.data)], axis=1)
             mergedScaledData = mergedScaledData[::-1]
             binsEdges = np.linspace(start=self.xmin, stop=self.xmax, num=self.bins)
-            mergedBinsEdges = b = np.tile(binsEdges, (len(self.data), 1)).T
+            mergedBinsEdges = np.tile(binsEdges, (len(self.data), 1)).T
             # both filled and outlined
             if self.style == "both":
                 self.ax.hist(x         = mergedBinsEdges,
